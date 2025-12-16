ed3eaf91743c49545512139247f9b1d79c78ee86
Yadhu <Yadhu@proton.me>
2025-12-16 19:22:04 +0530

    new

--- ElderProjectjr/ElderProjectjr/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ElderProjectjr.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
