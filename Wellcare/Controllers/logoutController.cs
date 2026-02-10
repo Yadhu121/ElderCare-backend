31580bf622c51d0d793a9a35b60aaea5f545884a
Yadhu <Yadhu@proton.me>
2026-02-10 16:47:13 +0530

    Location/live

---
+++ Wellcare/Controllers/logoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wellcare.Controllers
+{
+    public class logoutController : Controller
+    {
+        public IActionResult Logout()
+        {
+            //HttpContext.Session.Clear();
+            Response.Cookies.Delete("access_token");
+            return RedirectToAction("Login", "caretakerLogin");
+        }
+    }
+}
---
+++ Wellcare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wellcare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
